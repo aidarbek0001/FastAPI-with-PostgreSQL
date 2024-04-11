4f2ad9537dc83c25ffc8cba7d45fffbc3055604e
Aidar Molzhigit <aidarbek0001@gmail.com>
2024-04-11 20:12:23 +0500

    v2

--- database.py
+++ database.py
@@ -1,4 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('postgresql:')
+
+
+engine = create_engine('postgresql://postgres:Aa1234@localhost:5432/item_db', echo=True)
+
+Base = declarative_base()
+
+SessionLocal = sessionmaker(bind=engine)
+
--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 from pydantic import BaseModel
-
+from typing import List
+from database import SessionLocal
+import models
 
 app = FastAPI()
+
+# @app.get("/")
+# async def root():
+#    return {"message": "Hello World"}
+
+# @app.get("/hello/{name}")
+# async def say_hello(name: str):
+#    return {"message": f"Hello {name}"}
+
+# @app.put('/item/{item_id}')
+# def update_item(item: Item, item_id: int):
+#    return {'name': item.name,
+#            'description': item.description,
+#            'price': item.price,
+#            'on_offer': item.on_offer
+#            }
+
+db = SessionLocal()
 
 
 class Item(BaseModel):
@@ -12,22 +32,47 @@ class Item(BaseModel):
     price: int
     on_offer: bool
 
+    class Config:
+        orm_mode = True
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/items", response_model=List[Item], status_code=200)
+def get_all_items():
+    items = db.query(models.Item).all()
+    return items
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/item/{id}", response_model=Item, status_code=status.HTTP_200_OK)
+def get_an_item(item_id: int):
+    item = db.query(models.Item).filter(models.Item.id == item_id).first()
+    return item
 
 
-@app.put('/item/{item_id}')
-def update_item(item: Item, item_id: int):
-    return {'name': item.name,
-            'description': item.description,
-            'price': item.price,
-            'on_offer': item.on_offer
-            }
+@app.post("/items", response_model=Item, status_code=status.HTTP_201_CREATED)
+def create_an_item(item: Item):
+    new_item = models.Item(name=item.name, description=item.description,
+                           price=item.price, on_offer=item.on_offer)
+    db_item = db.query(models.Item).filter(item.name == new_item.name).first()
+
+    if db_item is not None:
+        raise HTTPException(status_code=400, detail="Item already exists")
+
+    db.add(new_item)
+    db.commit()
+    return new_item
+
+
+@app.put("/item/{id}", response_model=Item, status_code=status.HTTP_200_OK)
+def update_an_item(item_id: int, item: Item):
+    item_to_update = db.query(models.Item).filter(models.Item.id == item_id).first()
+    item_to_update.name = item.name
+    item_to_update.description = item.description
+    item_to_update.price = item.price
+    item_to_update.on_offer = item.on_offer
+
+    db.commit()
+    return item_to_update
+
+@app.delete("/item/{id}")
+def delete_an_item(item_id: int):
+    pass
